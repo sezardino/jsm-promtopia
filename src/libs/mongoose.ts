76c931a7b75e74e449aba7b0272f2c18f5dfe512
Edward Arechwa <e.arechwa@nebucode.com>
2023-06-11 16:57:49 +0200

    ft: add mongo connection

---
+++ src/libs/mongoose.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+let isMongoConnected = false;
+
+export const connectToMongo = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isMongoConnected) return;
+
+  const mongoUri = process.env.DB_URI!;
+  const mongoDbName = process.env.DB_NAME!;
+
+  try {
+    await mongoose.connect(mongoUri, {
+      dbName: mongoDbName,
+    });
+
+    isMongoConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
